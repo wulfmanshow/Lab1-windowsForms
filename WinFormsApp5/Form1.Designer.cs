a81b96558262aed91fe50019ac33bfff57a2ee16
Nikita-Poshtaruk <fanadeich@gmail.com>
2022-11-06 16:14:19 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp5/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace WinFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(131, 115);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(156, 111);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(465, 392);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ WinFormsApp5/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            int namelen,surnamelen,otchestvolen,sum,count=0;
+            
+            string name = string.Empty;
+            DialogResult result = MessageBox.Show(name = string.Format("Поштарюк Никита Сергеевич"), "Фио", MessageBoxButtons.OK);
+            count++;
+            namelen= name.Length;
+            Text = name;
+            string surname = string.Empty;
+            MessageBox.Show(surname = string.Format("Занимаюсь кодингом, баскет болом и играю в комп"), "О бо мне", MessageBoxButtons.OK);
+            count++;
+            surnamelen = surname.Length;
+            Text = surname;
+            string otchestvo = string.Empty;
+            MessageBox.Show(otchestvo = string.Format("Стать как минимум мидл бек енд розроботчиком"), "Цели", MessageBoxButtons.OK);
+            count++;
+            otchestvolen = otchestvo.Length;
+            double average = (namelen + surnamelen + otchestvolen)/3;
+            sum = namelen + surnamelen + otchestvolen;
+            Text = otchestvo;
+            MessageBox.Show($"среднее количество={average},сумма ={sum},количество сообщений={count}", $"Сумирование", MessageBoxButtons.OK);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
